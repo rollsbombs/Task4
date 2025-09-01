1a0220a5b0e0741400d2056d7ff408142d7d14f8
Eleonora Kuranova <aflyalf@gmail.com>
2025-09-01 20:06:36 +0300

    исправила задание 4.3

--- src/Task4/Task4_3.java
+++ src/Task4/Task4_3.java
@@ -5,31 +5,37 @@ import java.util.Scanner;
 public class Task4_3 {
 
     private static boolean isNumber(String str) {
-        if (str == null || str.isEmpty())
+        try {
+            Integer.parseInt(str);
+                return true;
+            } catch (NumberFormatException e) {
             return false;
-        for (int i = 0; i < str.length(); i++) {
-            char c  = str.charAt(i);
-            if (c<'0' || c>'9') {
-                return false;
-            }
         }
-        return true;
     }
 
     private static boolean isTrueDate(int day, int month, int year) {
         if (month < 1 || month > 12)
             return false;
-        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-            return day >= 1 && day <= 31;
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return day >= 1 && day <= 31;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return day >= 1 && day <= 30;
+            case 2:
+                boolean isVisYear = (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
+                return isVisYear ? day >= 1 && day <= 29 : day >= 1 && day <= 28;
+            default:
+                return false;
         }
-        if (month == 4 || month == 6 || month == 9 || month == 11) {
-            return day <= 30;
-        }
-        if (month == 2) {
-            boolean isVisYear = (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
-            return isVisYear ? day <= 29 : day <= 28;
-        }
-        return false;
     }
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
@@ -65,7 +71,7 @@ public class Task4_3 {
                 return;
             }
             if (!isTrueDate(day, month, year)) {
-                System.err.println("Ошибка: неверная дата");
+                System.err.println("Ошибка: такой даты не существует");
                 return;
             }
             String formatDay = String.format("%02d", day);
